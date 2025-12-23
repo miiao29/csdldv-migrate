8e5025ce6ca3c9e76d1a7f66f31769bc1659188a
miiao29 <95594868+miiao29@users.noreply.github.com>
2025-12-23 13:57:40 +0700

    Initial commit

---
+++ src/main/java/com/csdldv/CsdldvFixMirgateApplication.java
@@ -0,0 +1,13 @@
+package com.csdldv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdldvFixMirgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdldvFixMirgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csdldv/CsdldvFixMirgateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdldv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdldvFixMirgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
