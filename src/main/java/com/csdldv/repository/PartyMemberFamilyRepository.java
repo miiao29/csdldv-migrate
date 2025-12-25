f5bfaa8dc33efeba4ca524e56c03696ff8bd1c24
miiao29 <95594868+miiao29@users.noreply.github.com>
2025-12-25 13:58:46 +0700

    Update PartyMemberFamilyRepository.java

--- src/main/java/com/csdldv/repository/PartyMemberFamilyRepository.java
+++ src/main/java/com/csdldv/repository/PartyMemberFamilyRepository.java
@@ -5,49 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface PartyMemberFamilyRepository extends JpaRepository<PartyMemberFamily, String> {
-
-    @Modifying
-    @Query(value = """
-            UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY a
-            SET a.type =
-                CASE
-                    WHEN a.type IS NULL
-                         AND EXISTS (
-                             SELECT 1
-                             FROM CSDLDV_20.qhe_gd b
-                             WHERE b.guidkey = a.V3_QUANHE_GD_GUID
-                               AND b.MA_TV_GD IN (
-                                   'A2','A3','B5','B6','C7','C8',
-                                   'E2','E3','M2','M3','R1','R2'
-                               )
-                         )
-                          OR EXISTS (
-                              SELECT 1
-                              FROM CSDLDV_25.qhe_gd b
-                              WHERE b.guidkey = a.V3_QUANHE_GD_GUID
-                                AND b.MA_TV_GD IN (
-                                    'A2','A3','B5','B6','C7','C8',
-                                    'E2','E3','M2','M3','R1','R2'
-                                )
-                          )
-                          OR EXISTS (
-                              SELECT 1
-                              FROM CSDLDV_26.qhe_gd b
-                              WHERE b.guidkey = a.V3_QUANHE_GD_GUID
-                                AND b.MA_TV_GD IN (
-                                    'A2','A3','B5','B6','C7','C8',
-                                    'E2','E3','M2','M3','R1','R2'
-                                )
-                          )
-                        THEN 1
-                    ELSE 0
-                END
-            """, nativeQuery = true)
-    int updateFamilyType();
 
     @Query(value = """
             SELECT id FROM (
@@ -88,24 +50,18 @@ public interface PartyMemberFamilyRepository extends JpaRepository<PartyMemberFa
     List<String> findIdsForTypeOne(int batchSize);
 
     @Modifying
-    @Query(
-            value = """
-                    UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
-                    SET TYPE = 1
-                    WHERE PARTY_MEMBER_FAMILY_ID IN :ids
-                    """,
-            nativeQuery = true
-    )
+    @Query(value = """
+            UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
+            SET TYPE = 1
+            WHERE PARTY_MEMBER_FAMILY_ID IN :ids
+            """, nativeQuery = true)
     int bulkSetTypeOne(List<String> ids);
 
     @Modifying
-    @Query(
-            value = """
-                    UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
-                    SET TYPE = 0
-                    WHERE TYPE IS NULL
-                    """,
-            nativeQuery = true
-    )
+    @Query(value = """
+            UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
+            SET TYPE = 0
+            WHERE TYPE IS NULL
+            """, nativeQuery = true)
     int bulkSetTypeZero();
 }
