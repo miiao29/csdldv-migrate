cabc9b68b2b4e4439f9847acab3cdf6defa1e3df
miiao29 <95594868+miiao29@users.noreply.github.com>
2025-12-26 13:10:31 +0700

    update log

--- src/main/java/com/csdldv/batch/DisciplineMigrationService.java
+++ src/main/java/com/csdldv/batch/DisciplineMigrationService.java
@@ -23,8 +23,75 @@ public class DisciplineMigrationService {
     public void updateDisciplineFormAndReasonInBatches(int batchSize) {
         log.info("Starting PARTY_MEMBER_DISCIPLINE migration (MA_KL, LYDO) with batch size {}", batchSize);
 
+        String selectSql = """
+                SELECT x.PARTY_MEMBER_DISCIPLINE_ID
+                FROM CSDLDV_PARTY_MEMBER.PARTY_MEMBER_DISCIPLINE x
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM CSDLDV_20.KT_KL kl
+                    JOIN CSDLDV_CATEGORY.CATEGORY c
+                      ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                    WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                      AND kl.GUIDKEY = x.V3_KT_KL_GUID
+                )
+                OR EXISTS (
+                    SELECT 1
+                    FROM CSDLDV_25.KT_KL kl
+                    JOIN CSDLDV_CATEGORY.CATEGORY c
+                      ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                    WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                      AND kl.GUIDKEY = x.V3_KT_KL_GUID
+                )
+                OR EXISTS (
+                    SELECT 1
+                    FROM CSDLDV_26.KT_KL kl
+                    JOIN CSDLDV_CATEGORY.CATEGORY c
+                      ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                    WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                      AND kl.GUIDKEY = x.V3_KT_KL_GUID
+                )
+                ORDER BY x.PARTY_MEMBER_DISCIPLINE_ID
+                """;
+        log.info("SQL SELECT to find IDs:\n{}", selectSql);
+
         List<String> allIds = partyMemberDisciplineRepository.findAllIdsForUpdate();
         log.info("Found {} total records to update", allIds.size());
+
+        String updateSqlTemplate = """
+                UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_DISCIPLINE x
+                SET (x.DISCIPLINE_FORM_ID, x.DISCIPLINE_REASON) =
+                (
+                    SELECT s.CATEGORY_ID, s.LYDO
+                    FROM (
+                        SELECT c.CATEGORY_ID,
+                               kl.GUIDKEY,
+                               kl.LYDO
+                        FROM CSDLDV_CATEGORY.CATEGORY c
+                        JOIN CSDLDV_20.KT_KL kl
+                          ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                        WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                        UNION ALL
+                        SELECT c.CATEGORY_ID,
+                               kl.GUIDKEY,
+                               kl.LYDO
+                        FROM CSDLDV_CATEGORY.CATEGORY c
+                        JOIN CSDLDV_25.KT_KL kl
+                          ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                        WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                        UNION ALL
+                        SELECT c.CATEGORY_ID,
+                               kl.GUIDKEY,
+                               kl.LYDO
+                        FROM CSDLDV_CATEGORY.CATEGORY c
+                        JOIN CSDLDV_26.KT_KL kl
+                          ON ('0' || c.CATEGORY_CODE) = kl.MA_KL
+                        WHERE c.CATEGORY_GROUP_CODE = 'HINHTHUCKYLUAT'
+                    ) s
+                    WHERE s.GUIDKEY = x.V3_KT_KL_GUID
+                )
+                WHERE x.PARTY_MEMBER_DISCIPLINE_ID IN (:ids)
+                """;
+        log.info("SQL UPDATE template:\n{}", updateSqlTemplate);
 
         long totalUpdated = 0;
 
@@ -33,6 +100,10 @@ public class DisciplineMigrationService {
                 final int startIndex = i;
                 int endIndex = Math.min(i + batchSize, allIds.size());
                 List<String> batchIds = allIds.subList(i, endIndex);
+
+                String idsPlaceholder = String.join("','", batchIds);
+                String updateSql = updateSqlTemplate.replace(":ids", "'" + idsPlaceholder + "'");
+                log.info("SQL UPDATE for batch {} to {}:\n{}", startIndex + 1, endIndex, updateSql);
 
                 Integer updated = transactionTemplate.execute(status -> {
                     log.info("Updating batch {} to {} of {}", startIndex + 1, endIndex, allIds.size());
--- src/main/java/com/csdldv/batch/FamilyMigrationService.java
+++ src/main/java/com/csdldv/batch/FamilyMigrationService.java
@@ -23,6 +23,58 @@ public class FamilyMigrationService {
     public void migrateFamilyTypeInBatches(int batchSize) {
         log.info("Starting family type migration with batch size {}", batchSize);
 
+        String selectSql = String.format("""
+                SELECT id FROM (
+                    SELECT a.PARTY_MEMBER_FAMILY_ID id
+                    FROM CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY a
+                    WHERE a.TYPE IS NULL
+                      AND EXISTS (
+                          SELECT 1
+                          FROM CSDLDV_20.qhe_gd b
+                          WHERE b.guidkey = a.V3_QUANHE_GD_GUID
+                            AND b.MA_TV_GD IN (
+                                'A2','A3','B5','B6','C7','C8',
+                                'E2','E3','M2','M3','R1','R2'
+                            )
+                      )
+                       OR EXISTS (
+                           SELECT 1
+                           FROM CSDLDV_25.qhe_gd b
+                           WHERE b.guidkey = a.V3_QUANHE_GD_GUID
+                             AND b.MA_TV_GD IN (
+                                 'A2','A3','B5','B6','C7','C8',
+                                 'E2','E3','M2','M3','R1','R2'
+                             )
+                       )
+                       OR EXISTS (
+                           SELECT 1
+                           FROM CSDLDV_26.qhe_gd b
+                           WHERE b.guidkey = a.V3_QUANHE_GD_GUID
+                             AND b.MA_TV_GD IN (
+                                 'A2','A3','B5','B6','C7','C8',
+                                 'E2','E3','M2','M3','R1','R2'
+                             )
+                       )
+                    ORDER BY a.PARTY_MEMBER_FAMILY_ID
+                )
+                WHERE ROWNUM <= %d
+                """, batchSize);
+        log.info("SQL SELECT to find IDs (batch size {}):\n{}", batchSize, selectSql);
+
+        String updateTypeOneSqlTemplate = """
+                UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
+                SET TYPE = 1
+                WHERE PARTY_MEMBER_FAMILY_ID IN (:ids)
+                """;
+        log.info("SQL UPDATE template to set TYPE = 1:\n{}", updateTypeOneSqlTemplate);
+
+        String updateTypeZeroSql = """
+                UPDATE CSDLDV_PARTY_MEMBER.PARTY_MEMBER_FAMILY
+                SET TYPE = 0
+                WHERE TYPE IS NULL
+                """;
+        log.info("SQL UPDATE to set TYPE = 0:\n{}", updateTypeZeroSql);
+
         long totalUpdated = 0;
 
         try {
@@ -33,13 +85,21 @@ public class FamilyMigrationService {
                     break;
                 }
 
-                Integer updated = transactionTemplate.execute(status -> partyMemberFamilyRepository.bulkSetTypeOne(ids));
+                String idsPlaceholder = String.join("','", ids);
+                String updateTypeOneSql = updateTypeOneSqlTemplate.replace(":ids", "'" + idsPlaceholder + "'");
+                log.info("SQL UPDATE to set TYPE = 1 for batch:\n{}", updateTypeOneSql);
+
+                Integer updated = transactionTemplate.execute(status -> {
+                    log.info("Updating batch with {} IDs", ids.size());
+                    return partyMemberFamilyRepository.bulkSetTypeOne(ids);
+                });
 
                 totalUpdated += updated;
 
                 log.info("Batch updated {} rows, total updated {}", updated, totalUpdated);
             }
 
+            log.info("Executing SQL UPDATE to set TYPE = 0 for remaining records");
             Integer setZero = transactionTemplate.execute(status -> partyMemberFamilyRepository.bulkSetTypeZero());
 
             log.info("Finished family type migration, set type=1 {}, set type=0 {}", totalUpdated, setZero);
